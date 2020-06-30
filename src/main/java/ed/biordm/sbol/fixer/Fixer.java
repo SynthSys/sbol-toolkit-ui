df44bbacbbe30a3edfcdcb383f462e8b6c3b23b5
T. Zielinski <zajawka@o2.pl>
2020-07-01 00:17:10 +0100

    checking annotations

--- src/main/java/ed/biordm/sbol/fixer/Fixer.java
+++ src/main/java/ed/biordm/sbol/fixer/Fixer.java
@@ -8,6 +8,7 @@ package ed.biordm.sbol.fixer;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Set;
+import javax.xml.namespace.QName;
 import org.sbolstandard.core2.Component;
 import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.SBOLConversionException;
@@ -39,9 +40,40 @@ public class Fixer {
                     if (a.isStringValue()) {
                         System.out.println(a.getStringValue());
                     }
-                            
+                    System.out.println(a.getQName().getNamespaceURI());       
                 });
             }
+            
+            if (compd.getDisplayId().equals("IBMc202")) {
+                String value = "Name: IBMc202\n" +
+"Summary: pSB3T5(BsaI-)-PBAD(SapI-)-B30-mCherry*(1-192)-5aa-SYNZIP17-BCD1-SYNZIP18-5aa-mCherry*(193-236)-H6-Ter\n" +
+"Creator: Trevor Y. H. Ho\n" +
+"Principal Investigator: Baojun Wang\n" +
+"\n" +
+"BioSafety Level: Level 1\n" +
+"Backbone: pSB3T5\n" +
+"Origin of replication: p15A\n" +
+"Selection Markers: Tetracycline";
+                
+                QName qName = new QName("http://wiki.synbiohub.org/wiki/Terms/synbiohub#", 
+                        "mutableDescription");
+                
+                try {
+                    compd.createAnnotation(qName, value);
+                } catch (SBOLValidationException e) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+            }
+            
+            if (compd.getDisplayId().equals("IBMc202")) {
+                compd.getAnnotations().forEach( a -> {
+                    System.out.println(a.getQName());
+                    if (a.isStringValue()) {
+                        System.out.println(a.getStringValue());
+                    }
+                            
+                });
+            }            
             
         });
     }
