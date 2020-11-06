8128a0f4c39c2e41e8e9ff4a0cb2d7d346ae83b8
hayjohnny2000 <hayjohnny2000@yahoo.com>
2020-11-06 13:33:26 +0000

    Rough methods in SynBioHubClientController

--- src/main/java/ed/biordm/sbol/web/SynBioHubClientController.java
+++ src/main/java/ed/biordm/sbol/web/SynBioHubClientController.java
@@ -6,10 +6,12 @@
 package ed.biordm.sbol.web;
 
 import java.nio.charset.StandardCharsets;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -36,9 +38,24 @@ public class SynBioHubClientController {
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String LOGIN_URL = "https://synbiohub.org/login";
+    @Value("${synbiohub.client.user}")
+    private String synBioHubUser;
+
+    @Value("${synbiohub.client.email}")
+    private String synBioHubEmail;
+
+    @Value("${synbiohub.client.pass}")
+    private String synBioHubPass;
+
+    /*private final String LOGIN_URL = "https://synbiohub.org/login";
     private final String USER_API = "https://synbiohub.org/users";
-    private final String SUBMIT_API = "https://synbiohub.org/submit";
+    private final String SUBMIT_API = "https://synbiohub.org/submit";*/
+
+    private final String LOGIN_URL = "http://localhost:7777/login";
+    private final String USER_API = "http://localhost:7777/users";
+    private final String SUBMIT_API = "http://localhost:7777/submit";
+
+    HttpHeaders headers = new HttpHeaders();
 
     public SynBioHubClientController(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
@@ -62,14 +79,31 @@ public class SynBioHubClientController {
         return restTemplate.getForObject(USER_API, User[].class);
     }
 
+    public void doLogin() {
+        /*String credentials = synBioHubUser+":"+synBioHubPass;
+        byte[] credentialBytes = credentials .getBytes();
+        byte[] base64CredentialBytes = Base64.encodeBase64(credentialBytes);
+        String base64Credentials = new String(base64CredentialBytes);
+        headers.add("Authorization", "Basic " + base64Credentials );*/
+        final ResponseEntity<String> responseEntity = restTemplate
+                .exchange(LOGIN_URL, HttpMethod.POST, new HttpEntity<Void>(createHeaders(synBioHubEmail, synBioHubPass)), String.class);
+        System.out.println(responseEntity.getBody());
+    }
+
     @RequestMapping(path = "/submit", method = RequestMethod.POST)
     public ModelAndView doSubmits() {
         var mav = new ModelAndView();
 
-        System.out.println("WTTTTFFFFFF!!!!!!!!");
-        logger.warn("WTTTTFFFFFF!!!!!!!!");
+        if(!headers.containsKey("Authorization")) {
+            doLogin();
+        }
+
+        logger.debug("WTTTTFFFFFF?!");
+
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+
         final ResponseEntity<String> responseEntity = restTemplate
-                .exchange(SUBMIT_API, HttpMethod.POST, new HttpEntity<Void>(createHeaders("admin", "password")), String.class);
+                .exchange(SUBMIT_API, HttpMethod.POST, new HttpEntity<Void>(createHeaders(synBioHubUser, synBioHubPass)), String.class);
         System.out.println(responseEntity.getBody());
         mav.setViewName("submit-result");
 
